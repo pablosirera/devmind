5fa6279d97c9f5dc832ca28a2edb2ca80a9030c2
Pablo Sirera <psirera4@gmail.com>
2020-10-27 20:24:48 +0100

    feat: create login page

---
+++ pages/auth/login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="h-full overflow-hidden">
+    <div class="flex justify-end p-4">
+      <ColorSwitch />
+    </div>
+    <h1 class="text-center text-3xl font-bold">Devmind</h1>
+
+    <div
+      class="login-body mt-12 pt-56 h-full flex justify-center bg-light dark:bg-blue-main-800"
+    >
+      <p>iniciar sesión con google</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import ColorSwitch from '~/components/ui/ColorSwitch.vue'
+
+export default {
+  name: 'Login',
+  layout: 'auth',
+  components: {
+    ColorSwitch,
+  },
+}
+</script>
+
+<style scoped>
+.login-body {
+  width: 200%;
+  margin-left: -50%;
+  border-top-right-radius: 50%;
+  border-top-left-radius: 50%;
+}
+</style>
