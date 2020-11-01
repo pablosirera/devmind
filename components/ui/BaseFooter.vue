992e162ff598f2df4af01ea2b3dd808490b4ff9e
Pablo Sirera <psirera4@gmail.com>
2020-11-01 23:18:37 +0100

    fix(global): update colors and footer style

--- components/ui/BaseFooter.vue
+++ components/ui/BaseFooter.vue
@@ -1,15 +1,16 @@
 <template>
   <footer
-    class="footer flex justify-around px-5 pt-2 dark:text-white dark:bg-blue-main-800"
+    class="footer flex justify-around px-5 pt-2 text-gray-main-700 dark:text-gray-main-800 dark:bg-blue-main-800"
   >
     <nuxt-link
       v-for="(link, index) in links"
       :key="index"
       :to="link.url"
       class="flex flex-col items-center"
+      :class="`link-${$colorMode.preference}`"
     >
       <!-- TODO: change to dark when is light mode -->
-      <component :is="link.component" class="w-8 h-8 fill-white" />
+      <component :is="link.component" class="w-8 h-8" />
       <span>{{ link.name }}</span>
     </nuxt-link>
   </footer>
@@ -48,5 +49,29 @@ export default {
 <style scoped>
 .footer {
   height: var(--footer);
+}
+
+.link-dark svg {
+  fill: theme('colors.gray-main.800');
+}
+
+.link-light svg {
+  fill: theme('colors.gray-main.700');
+}
+
+.nuxt-link-exact-active {
+  color: theme('colors.gray-main.900');
+}
+
+.nuxt-link-exact-active.link-dark {
+  color: theme('colors.white');
+}
+
+.nuxt-link-exact-active svg {
+  fill: theme('colors.gray-main.900');
+}
+
+.nuxt-link-exact-active.link-dark svg {
+  fill: theme('colors.white');
 }
 </style>
